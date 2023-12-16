7d9393723bdb4705509ca42893367267a31857d8
SWLee2973 <kek2973@naver.com>
2023-12-16 12:19:23 +0900

    [step 2] 이미지 및 텍스트 변경

--- mission02/poster/client/js/main.js
+++ mission02/poster/client/js/main.js
@@ -9,6 +9,7 @@
 */
 
 const navigation = document.querySelector("nav");
+const visualImage = document.querySelector('.visual img');
 
 // 1-1. 클릭 이벤트 활성화 - 이벤트 지정
 navigation.addEventListener("click", function (e) {
@@ -29,5 +30,9 @@ navigation.addEventListener("click", function (e) {
   // 2. nav 클릭 시 배경 색상 변경
   document.body.style.background = `linear-gradient(to bottom, ${data[index - 1].color[0]},${data[index - 1].color[1]})`;
 
+  // 3. 이미지 변경
+  visualImage.src = `./assets/${data[index - 1].name.toLowerCase()}.jpeg`;
+  // 4. 텍스트 변경
+  visualImage.alt = `${data[index - 1].alt}`;
 });
 
