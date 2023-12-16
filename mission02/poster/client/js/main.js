448b1e4caff8d635303a9df119f0d4b3ea6ab69e
SWLee2973 <kek2973@naver.com>
2023-12-16 12:07:39 +0900

    [step1] nav 클릭 이벤트 활성화 및 배경 색상 변경

--- mission02/poster/client/js/main.js
+++ mission02/poster/client/js/main.js
@@ -1,4 +1,3 @@
-
 /* 
 
 1. 클릭 이벤트 활성화
@@ -9,16 +8,26 @@
 
 */
 
+const navigation = document.querySelector("nav");
 
+// 1-1. 클릭 이벤트 활성화 - 이벤트 지정
+navigation.addEventListener("click", function (e) {
+  e.preventDefault();
+  // 1-2. 클릭 이벤트 활성화
+  // console.log(e.target);
+  // console.log(e.currentTarget);
 
+  let li = e.target.closest("li");
+  if (!li) return;
 
+  // 1-3. 클릭 이미지 테두리
+  Array.from(li.parentElement.children).forEach(item => item.classList.remove('is-active'));
+  li.classList.add('is-active');
 
+  let index = li.dataset.index;
 
+  // 2. nav 클릭 시 배경 색상 변경
+  document.body.style.background = `linear-gradient(to bottom, ${data[index - 1].color[0]},${data[index - 1].color[1]})`;
 
-
-
-
-
-
-
+});
 
