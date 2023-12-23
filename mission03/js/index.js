6547c747fb4b02a329923fb95f4419d8b0de3231
SWLee2973 <kek2973@naver.com>
2023-12-24 02:01:35 +0900

    [develop] 스크롤바 디자인, 동작 버튼 추가

--- mission03/js/index.js
+++ mission03/js/index.js
@@ -1,8 +1,11 @@
 
+const navBar = document.querySelectorAll('.navbar button');
+const [loop, prev, play, next, shuffle] = navBar;
+
 
 const swiper = new Swiper('.swiper', {
-  loop: true,
-  // effect: "cube",
+  // loop: true,
+  effect: "cube",
   cubeEffect: {
     shadow: false,
     slideShadows: true,
@@ -17,5 +20,34 @@ const swiper = new Swiper('.swiper', {
   }
 });
 
+let loopFlag = 0;
 
+function handleLoop(e) {
 
+  if(loopFlag) {
+
+  }
+
+}
+
+function handlePrev(e) {
+
+}
+
+function handlePlay(e) {
+}
+
+function handleNext(e) {
+  swiper.next();
+
+}
+
+function handleShuffle(e) {
+
+}
+
+loop.addEventListener('click', handleLoop);
+prev.addEventListener('click', handlePrev);
+play.addEventListener('click', handlePlay);
+next.addEventListener('click', handleNext);
+shuffle.addEventListener('click', handleShuffle);
