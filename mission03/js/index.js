d200419b754173ec0b995d8100fde013e0062c28
SWLee2973 <kek2973@naver.com>
2023-12-26 08:41:02 +0900

    [modify] 슬라이드 효과 fade로 변경

--- mission03/js/index.js
+++ mission03/js/index.js
@@ -83,13 +83,13 @@ async function renderPlaylist() {
     swiper = new Swiper('.swiper', {
       loop: true,
       touchRatio: 0,
-      // effect: "cube",
-      cubeEffect: {
-        shadow: false,
-        slideShadows: true,
-        shadowOffset: 20,
-        shadowScale: 0.94,
-      },
+      effect: "fade",
+      // cubeEffect: {
+      //   shadow: false,
+      //   slideShadows: true,
+      //   shadowOffset: 20,
+      //   shadowScale: 0.94,
+      // },
       // autoplay: true,
       parallax: true,
       speed: 2000,
